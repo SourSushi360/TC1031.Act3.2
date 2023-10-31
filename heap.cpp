40e0029393084560e56dbfa89a42a1c9b3b78cf3
SourSushi360 <jplopez365@hotmail.com>
2023-10-31 17:23:36 -0600

    added print

--- heap.cpp
+++ heap.cpp
@@ -5,6 +5,7 @@
     este programa define las funciones de la clase heap, declarada
     en heap.hpp */
 #include "heap.hpp"
+#include <iostream>
 
 // constructores
 Heap::Heap() {};
@@ -61,4 +62,56 @@ bool Heap::isEmpty() {
 }
 int Heap::size() {
     return heapArray.size();
+}
+void Heap::print (int option) {
+    switch (option) {
+        case 1:
+            preorder(0,heapArray);
+            break;
+        case 2:
+            inorder(0,heapArray);
+            break;
+        case 3:
+            postorder(0,heapArray);
+            break;
+        case 4:
+            levelByLevel(heapArray);
+            break;
+        default:
+            std::cout << "opcion invalida\n";
+    }
+}
+// print helper
+void preorder(int index,std::vector<int> heapArray) {
+    if (index < heapArray.size()) {
+        // Print the current element
+        std::cout << heapArray[index] << " ";
+        // Recursively traverse the left and right subtrees
+        preorder(2 * index + 1,heapArray);
+        preorder(2 * index + 2,heapArray);
+    }
+}
+void inorder(int index,std::vector<int> heapArray) {
+    if (index < heapArray.size()) {
+        // Recursively traverse the left subtree
+        inorder(2 * index + 1,heapArray);
+        // Print the current element
+        std::cout << heapArray[index] << " ";
+        // Recursively traverse the right subtree
+        inorder(2 * index + 2,heapArray);
+    }
+}
+void postorder(int index,std::vector<int> heapArray) {
+    if (index < heapArray.size()) {
+        // Recursively traverse the left and right subtrees
+        postorder(2 * index + 1,heapArray);
+        postorder(2 * index + 2,heapArray);
+        // Print the current element
+        std::cout << heapArray[index] << " ";
+    }
+}
+void levelByLevel(std::vector<int> heapArray) {
+    for (int i = 0; i < heapArray.size(); i++) {
+        std::cout << heapArray[i] << " ";
+    }
 }
